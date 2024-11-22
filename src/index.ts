74e807c94c1d4dee7f8a956668a8df8f4b482e30
steves3142 <helixdeveloper1@gmail.com>
2024-11-22 17:58:00 -0500

    full backend

--- src/index.ts
+++ src/index.ts
@@ -20,3 +20,22 @@ app.use(
 		credentials: true,
 	})
 );
+
+app.post('/translateDocument', async (c) => {
+	const { documentData, targetLang } = await c.req.json();
+
+	//Generate a summary of the document
+	const summaryResponse = await c.env.AI.run('@cf/facebook/bart-large-cnn', {
+		input_text: documentData,
+		max_length: 1000,
+	});
+
+	//translate the summary into another language
+	const response = await c.env.AI.run('@cf/meta/m2m100-1.2b', {
+		text: summaryResponse.summary,
+		source_lang: 'english',
+		target_lang: targetLang,
+	})
+
+	return new Response(JSON.stringify(response));
+})
