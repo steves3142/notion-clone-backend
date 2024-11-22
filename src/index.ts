c75e633d98de9e82045956c49e6e43660b0f1bcb
steves3142 <helixdeveloper1@gmail.com>
2024-11-22 17:19:37 -0500

    enabling Bingdings and modifying allwed headers to fix CORS

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from "hono";
+import { OpenAI } from "openai";
+import { cors } from "hono/cors";
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+type Bindings = {
+	OPEN_AI_KEY: string;
+	AI: Ai;
+}
+
+const app = new Hono<{Bindings: Bindings}>();
+
+app.use(
+	'/*',
+	cors({
+		origin: '*', //Allow requests from Next.JS app
+		allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-type'], //Add Content-Type to the allowed headers to fix CORS
+		allowMethods: ['POST', 'GET', 'OPTIONS', 'PUT'],
+		exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+		maxAge: 600,
+		credentials: true,
+	})
+);
